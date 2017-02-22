d9ac8a2871379c4beb132474e420df3e16e6f865
panforong <18702598616@163.com>
2017-02-22 17:48:25 +0800

    <添加直播逻辑>

--- app/src/main/java/cn/panfr/pandalive/live/LiveHelper.java
+++ app/src/main/java/cn/panfr/pandalive/live/LiveHelper.java
@@ -9,7 +9,44 @@ import android.graphics.Bitmap;
 public class LiveHelper {
     public static native String helloFromFFmpeg();
 
+    /**
+     * libjpeg压缩图片方法
+     * @param bitmap 原位图
+     * @param width 需要压缩的宽度
+     * @param height 需要压缩的高度
+     * @param quality 压缩率
+     * @param fileName 保存的文件地址
+     * @param optimize 是否开始哈弗曼算法
+     * @return 0 成功 1 失败
+     */
     public static native String compressBitmap(Bitmap bitmap, int width, int height, int quality, byte[] fileName, boolean optimize);
+
+    /**
+     * 初始化编码
+     * @param width 编码视频的宽度
+     * @param height 编码视频的高度
+     * @return 0 成功  小于0失败
+     */
+    public static native int streamerInit(int width, int height, String address);
+
+    /**
+     * 对每一帧进行编码推流
+     * @param data NV21格式的数据
+     * @return 0成功，小于0失败
+     */
+    public static native int streamerHandle(byte[] data);
+
+    /**
+     * 把缓冲帧的数据清空
+     * @return 0成功，小于0失败
+     */
+    public static native int streamerFlush();
+
+    /**
+     * 释放资源，比如编码器这些
+     * @return 0成功，小于0失败
+     */
+    public static native int streamerRelease();
 
     static {
         System.loadLibrary("jpeg");// libjpeg
--- app/src/main/java/cn/panfr/pandalive/ui/LiveActivity.java
+++ app/src/main/java/cn/panfr/pandalive/ui/LiveActivity.java
@@ -3,13 +3,21 @@ package cn.panfr.pandalive.ui;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.lang.ref.WeakReference;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.panfr.pandalive.R;
+import cn.panfr.pandalive.live.LiveHelper;
 
 public class LiveActivity extends AppCompatActivity implements SurfaceHolder.Callback, Camera.PreviewCallback{
 
@@ -22,6 +30,20 @@ public class LiveActivity extends AppCompatActivity implements SurfaceHolder.Cal
     private int width = 1280;
     private int height = 720;
 
+    private HandlerThread mHandlerThread;
+
+    private LiveHandler mHandler;
+
+
+    private static final int STREAMER_INIT = 1;
+    private static final int STREAMER_HANDLE = 2;
+    private static final int STREAMER_FLUSH = 3;
+    private static final int STREAMER_RELEASE = 4;
+
+    /**
+     * 判断有没有初始化成功，不成功不不进行后续的编码处理
+     */
+    private int liveInitResult = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +54,34 @@ public class LiveActivity extends AppCompatActivity implements SurfaceHolder.Cal
         mSurfaceHolder.setFixedSize(width, height);
         mSurfaceHolder.addCallback(this);
 
+        mHandlerThread = new HandlerThread("liveHandlerThread");
+        mHandlerThread.start();
+        mHandler = new LiveHandler(this, mHandlerThread.getLooper());
     }
 
     @Override
-    public void onPreviewFrame(byte[] bytes, Camera camera) {
-
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        /**
+         * 如果初始化成功，那就把数据发送到Handler，然后再调用native方法
+         */
+        if(liveInitResult == 0 && data != null && data.length > 0) {
+            Message msg = Message.obtain();
+            Bundle bundle = new Bundle();
+            bundle.putByteArray("frame_data", data);
+            msg.what = STREAMER_HANDLE;
+            msg.setData(bundle);
+            mHandler.sendMessage(msg);
+        }
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        liveInitResult = streamerInit(width, height);
+        if(liveInitResult == -1) {
+            mHandler.sendEmptyMessage(STREAMER_RELEASE);
+        } else {
+            Log.e("LiveActivity", "streamer init result: " + liveInitResult);
+        }
         initCamera();
     }
 
@@ -51,6 +92,16 @@ public class LiveActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        /**
+         * 在surface销毁的时候清空缓冲帧（在直播成功开启的情况下）
+         * 清空后就进行资源的释放
+         * 并且把HandlerThread退出
+         */
+        if(liveInitResult == 0) {
+            mHandler.sendEmptyMessage(STREAMER_FLUSH);
+        }
+        mHandler.sendEmptyMessage(STREAMER_RELEASE);
+        mHandlerThread.quitSafely();
         destroyCamera();
     }
 
@@ -82,5 +133,65 @@ public class LiveActivity extends AppCompatActivity implements SurfaceHolder.Cal
         mCamera.stopPreview();
         mCamera.release();
         mCamera = null;
+    }
+    private static class LiveHandler extends Handler{
+        private WeakReference<LiveActivity> mActivity;
+
+        public LiveHandler(LiveActivity activity, Looper looper) {
+            super(looper);
+            mActivity = new WeakReference<LiveActivity>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            LiveActivity activity = mActivity.get();
+            if(activity == null) {
+                return;
+            }
+
+            switch (msg.what){
+                case STREAMER_INIT:
+                    break;
+
+                case STREAMER_HANDLE:
+                    Bundle bundle = msg.getData();
+                    if(bundle != null) {
+                        byte[] data = bundle.getByteArray("frame_data");
+                        if(data != null && data.length > 0) {
+                            activity.streamerHandle(data);
+                        } else {
+                            Log.e("LiveActivity", "byte data null");
+                        }
+                    } else {
+                        Log.e("LiveActivity", "bundle null");
+                    }
+                    break;
+
+                case STREAMER_FLUSH:
+                    activity.streamerFlush();
+                    break;
+
+                case STREAMER_RELEASE:
+                    activity.streamerRelease();
+                    break;
+            }
+        }
+    }
+
+    private int streamerInit(int width, int height) {
+        return LiveHelper.streamerInit(width, height,"rtmp://live.simope.com:1935/live/90a3c65d07?liveID=100029116&accessCode=f43348c2");
+    }
+
+    private void streamerRelease() {
+        LiveHelper.streamerRelease();
+    }
+
+    private void streamerFlush() {
+        LiveHelper.streamerFlush();
+    }
+
+    private void streamerHandle(byte[] data) {
+        LiveHelper.streamerHandle(data);
     }
 }
